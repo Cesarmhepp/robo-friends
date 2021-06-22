07c0ff5ef72e98753cb552c3c7e0c8b43e26ef03
cesar muñoz <cesarmhepp@gmail.com>
2021-06-22 16:36:51 -0400

    robo friends add

---
+++ src/robots.js
@@ -0,0 +1,40 @@
+export const robots=[
+    {
+        id:1,
+        name: 'Cesar Munoz',
+        username: 'cesar',
+        email:'cesar@cesar.com'
+    },
+    {
+        id:2,
+        name: 'Maria paz',
+        username: 'maria',
+        email:'cesar@cesar.com'
+    },
+    {
+        id:3,
+        name: 'almendra',
+        username: 'alme',
+        email:'cesar@cesar.com'
+    },
+    {
+        id:4,
+        name: 'leo',
+        username: 'alme',
+        email:'cesar@cesar.com'
+    }
+    ,
+    {
+        id:5,
+        name: 'jose',
+        username: 'alme',
+        email:'cesar@cesar.com'
+    }
+    ,
+    {
+        id:6,
+        name: 'juan',
+        username: 'alme',
+        email:'cesar@cesar.com'
+    }
+]
